2d98062da57a118a2c8a4f6709bc371eacc91b6f
zzangin <zzangin1919@gmail.com>
2024-12-16 20:43:56 +0900

    초기 커밋.

---
+++ NetTcpManager/Client/NetTcpClientManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTcpManager.Client
+{
+	public class NetTcpClientManager
+	{
+		#region => Field
+		#endregion => Field
+
+		#region => Property
+		#endregion => Property
+
+		#region => Constructor
+		#endregion => Constructor
+
+		#region => Method
+		#endregion => Method
+	}
+}
