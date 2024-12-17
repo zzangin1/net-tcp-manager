63feab8d5ccc6dcbbca9b778a667e0c8e3450e9b
zzangin <zzangin1919@gmail.com>
2024-12-17 19:11:23 +0900

    SendMessage 구조 정의

---
+++ NetTcpManager/Model/SendMessage.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+
+namespace NetTcpManager.Model
+{
+	/// <summary>
+	/// SendMessage 구조
+	/// </summary>
+	public class SendMessage
+	{
+		#region => Field
+		#endregion => Field
+
+		#region => Property
+
+		public string Message { get; set; }
+
+		public Socket Client { get; set; }
+
+		#endregion => Property
+
+		#region => Constructor
+
+		public SendMessage(Socket client, string message)
+		{
+			Message = message;
+			Client = client;
+		}
+
+		#endregion => Constructor
+
+		#region => Method
+		#endregion => Method
+	}
+}
