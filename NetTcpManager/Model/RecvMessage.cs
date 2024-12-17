6708f5cae25b9d936b16ca518fe3654f8444b873
zzangin <zzangin1919@gmail.com>
2024-12-17 19:11:14 +0900

    RecvMessage 구조 정의

---
+++ NetTcpManager/Model/RecvMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTcpManager.Model
+{
+	/// <summary>
+	/// RecvMessage 구조
+	/// </summary>
+	public class RecvMessage
+	{
+		#region => Field
+		#endregion => Field
+
+		#region => Property
+
+		public Guid MessageId { get; set; }
+
+		public string Message { get; set; }
+
+		#endregion => Property
+
+		#region => Constructor
+
+		public RecvMessage(string message)
+		{
+			MessageId = Guid.NewGuid();
+			Message = message;
+		}
+
+		#endregion => Constructor
+
+		#region => Method
+		#endregion => Method
+	}
+}
